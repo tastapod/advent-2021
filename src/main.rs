e5232d1df317c02165200eada1824a3aa5d3631d
Daniel Terhorst-North <daniel@dannorth.net>
2021-12-27 23:55:41 +0000

    move Day 1 code into day1 module

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
 mod day1;
 
 fn main() {
-    println!("{}", count_increases(day1::part1::report));
-}
-
-pub fn count_increases(report: &[i32]) -> i32 {
-    report
-        .iter()
-        .zip(report[1..].iter())
-        .fold(0, |acc, (l, r)| if r > l { acc + 1 } else { acc })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::count_increases;
-
-    #[test]
-    fn counts_number_of_increases() {
-        let report: &[i32] = &[199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(count_increases(report), 7);
-    }
+    println!("Day 1 part 1: {}", day1::count_increases(day1::REPORT));
 }
