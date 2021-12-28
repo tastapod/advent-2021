e6ec39b06b256c22ccaa0a6dd0d7091def8e39e6
Daniel Terhorst-North <daniel@dannorth.net>
2021-12-28 14:43:54 +0000

    extract Day 1 input into its own file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod day1;
 
 fn day1() {
-    println!("Day 1 part 1: {}", day1::count_increases(day1::REPORT)); // 1393
+    let report = day1::input();
+    println!("Day 1 part 1: {}", day1::count_increases(&report)); // 1393
     println!(
         "Day 1 part 2: {}",
-        day1::count_increases(day1::sum_triples(day1::REPORT).as_slice())
+        day1::count_increases(&day1::sum_triples(&report))
     ); // 1359
 }
 
